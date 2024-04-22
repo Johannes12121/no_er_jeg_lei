c19ecabc8b2cd4be4fc8e075935faef32693c9bb
Johannes12121 <josae4857@oslomet.no>
2024-04-22 18:32:12 +0200

    f

---
+++ src/main/java/oslomettest1/no_er/NoErApplication.java
@@ -0,0 +1,13 @@
+package oslomettest1.no_er;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoErApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoErApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oslomettest1/no_er/NoErApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomettest1.no_er;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoErApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
